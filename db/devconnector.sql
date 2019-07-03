48b7cc9f93d910d39123bc94c5cd73ad8e3b899a
Taimoor Farooq <taimour.farooq@nexuscorp-ltd.com>
2019-07-03 22:26:35 +0500

    profile route added

--- db/devconnector.sql
+++ db/devconnector.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2019 at 07:01 PM
+-- Generation Time: Jul 03, 2019 at 07:23 PM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.1.18
 
@@ -21,6 +21,86 @@ SET time_zone = "+00:00";
 --
 -- Database: `devconnector`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `id` int(11) NOT NULL,
+  `school` varchar(255) DEFAULT NULL,
+  `degree` varchar(255) DEFAULT NULL,
+  `fieldofstudy` varchar(255) DEFAULT NULL,
+  `fromdate` varchar(255) DEFAULT NULL,
+  `todate` varchar(255) DEFAULT NULL,
+  `currentdate` tinyint(1) DEFAULT NULL,
+  `description` text,
+  `profileid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experience`
+--
+
+CREATE TABLE `experience` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `fromdate` varchar(255) DEFAULT NULL,
+  `todate` varchar(255) DEFAULT NULL,
+  `currentdate` tinyint(1) DEFAULT NULL,
+  `description` text,
+  `profileid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL,
+  `handle` varchar(40) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `website` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `skills` text NOT NULL,
+  `bio` text NOT NULL,
+  `githubusername` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`id`, `handle`, `company`, `website`, `location`, `status`, `skills`, `bio`, `githubusername`, `date`, `userid`) VALUES
+(1, '', 'company', 'website', 'location', 'status', '', 'bio', 'githubusername3', '0000-00-00', 3),
+(2, '', 'company', 'website', 'location', 'status', '', 'bio', 'githubusername3', '0000-00-00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social`
+--
+
+CREATE TABLE `social` (
+  `id` int(11) NOT NULL,
+  `youtube` varchar(255) DEFAULT NULL,
+  `twitter` varchar(255) DEFAULT NULL,
+  `facebook` varchar(255) DEFAULT NULL,
+  `linkedin` varchar(255) DEFAULT NULL,
+  `instagram` varchar(255) DEFAULT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -43,11 +123,41 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `date`, `avatar`) VALUES
 (2, 'Taimoor Farooq', 'taimoorfarooq@outlook.com', '$2a$10$tiCr3eIsluWgBb9RbmF32eYq9udZ2CHyIORsPSOqQ7RHGg.XkXaHe', '2019-06-25', '//www.gravatar.com/avatar/fda3f84df69e02cdf0010a3c6c51246a?s=200&r=pg&d=mm'),
-(3, 'Mehroz Farooq', 'mehroz@gmail.com', '$2a$10$1YCSSlN8e3LHSBJ86RAVSuonibpe9C0gJ0vUC6PnGQUoQVsW63w2O', '2019-06-27', '//www.gravatar.com/avatar/5037d552045c685eaf6b29f9b03544d1?s=200&r=pg&d=mm');
+(3, 'Mehroz Farooq', 'mehroz@gmail.com', '$2a$10$1YCSSlN8e3LHSBJ86RAVSuonibpe9C0gJ0vUC6PnGQUoQVsW63w2O', '2019-06-27', '//www.gravatar.com/avatar/5037d552045c685eaf6b29f9b03544d1?s=200&r=pg&d=mm'),
+(4, 'Hammad Farooq', 'hammad@gmail.com', '$2a$10$NOewMhLz8JR2mm54tSfKzumauviQzL2i1YV6wCp3QwTB2xsY51PP.', '2019-07-02', '//www.gravatar.com/avatar/54491cbb90afcb9e8ad62b3257c2b183?s=200&r=pg&d=mm'),
+(5, 'Jawwad Farooq', 'jawwad@gmail.com', '$2a$10$z5K2QdLYI/C0.Z2xR0E46OIbwmKH2REwsIfV9Z/vXKktGFQjHJ8BK', '2019-07-02', '//www.gravatar.com/avatar/1700e526ccb6645061c690aafb8630eb?s=200&r=pg&d=mm');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `education`
+--
+ALTER TABLE `education`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_education` (`profileid`);
+
+--
+-- Indexes for table `experience`
+--
+ALTER TABLE `experience`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_experience` (`profileid`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_profile` (`userid`);
+
+--
+-- Indexes for table `social`
+--
+ALTER TABLE `social`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_social` (`userid`);
 
 --
 -- Indexes for table `users`
@@ -60,10 +170,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `education`
+--
+ALTER TABLE `education`
+  ADD CONSTRAINT `user_education` FOREIGN KEY (`profileid`) REFERENCES `profile` (`id`);
+
+--
+-- Constraints for table `experience`
+--
+ALTER TABLE `experience`
+  ADD CONSTRAINT `user_experience` FOREIGN KEY (`profileid`) REFERENCES `profile` (`id`);
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `user_profile` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `social`
+--
+ALTER TABLE `social`
+  ADD CONSTRAINT `user_social` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,102 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../config/Database');
+const passport = require('passport');
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"}));
+
+// @route   GET api/profile
+// @desc    Get Current User's profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+
+    db.query('SELECT COUNT(*) AS cnt FROM profile WHERE userid = ? ', req.user.id, function(err, data){
+        if(err) {
+            console.log(err);
+        } else {
+            // Check User
+            if(!data[0].cnt > 0) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+
+            res.json(data[0]);
+        }
+    });
+});
+
+// @route   GET api/profile
+// @desc    Create or Edit User profile
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const profileFields = {};
+    profileFields.userid = req.user.id;
+    if(req.body.handle) profileFields.handle = req.body.handle;
+    if(req.body.company) profileFields.company = req.body.company;
+    if(req.body.website) profileFields.website = req.body.website;
+    if(req.body.location) profileFields.location = req.body.location;
+    if(req.body.bio) profileFields.bio = req.body.bio;
+    if(req.body.status) profileFields.status = req.body.status;
+    if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+    // Skills - Split into array
+    if(typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',');
+    }
+    // Social Links
+    const profileSocial = {};
+    if(req.body.youtube) profileSocial.youtube = req.body.youtube;
+    if(req.body.twitter) profileSocial.twitter = req.body.twitter;
+    if(req.body.facebook) profileSocial.facebook = req.body.facebook;
+    if(req.body.linkedin) profileSocial.linkedin = req.body.linkedin;
+    if(req.body.instagram) profileSocial.instagram = req.body.instagram;
+
+    // console.log(profileFields);
+
+    db.query('SELECT COUNT(*) AS cnt FROM profile WHERE userid = ? ', req.user.id, function(err, data){
+        if(err) {
+            console.log(err);
+        } else {
+            // Edit Profile
+            if(data[0].cnt > 0) {
+                db.query('UPDATE profile SET ? WHERE userid = ?',[profileFields, req.user.id], function(err, result){
+                    if(err) console.log(err);
+
+                    res.json({'msg':'Profile Updated'})
+                });
+            } else {
+                db.query('INSERT INTO profile SET ?', profileFields, function(err, result){
+                    if(err) console.log(err);
+
+                    res.json({'msg':'Profile Created'});
+                });
+            }
+        }
+    });
+    
+    db.query('SELECT COUNT(*) AS cnt FROM social WHERE userid = ? ', req.user.id, function(err, data){
+        if(err) {
+            console.log(err);
+        } else {
+            // Edit Profile
+            if(data[0].cnt > 0) {
+                db.query('UPDATE social SET ? WHERE userid = ?', [profileSocial, req.user.id], function(err, result){
+                    if(err) console.log(err);
+
+                    res.json({'msg':'Profile Updated'})
+                });
+            } else {
+                db.query('INSERT INTO profile SET ? ', profileSocial, function(err, result){
+                    if(err) console.log(err);
+
+                    res.json({'msg':'Profile Created'});
+                });
+            }
+        }
+    });
+});
 
 module.exports = router;
